de2cc4357b7e69a4819ec12db3e13366b440e9f8
weekbelt <vfrvfr4207@gmail.com>
2022-03-05 15:41:05 +0900

    리팩토링 10. 함수를 명령으로 바꾸기

--- src/main/java/me/whiteship/refactoring/_03_long_function/_10_replace_function_with_command/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_10_replace_function_with_command/StudyDashboard.java
@@ -1,19 +1,15 @@
 package me.whiteship.refactoring._03_long_function._10_replace_function_with_command;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueComment;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 public class StudyDashboard {
 
@@ -26,17 +22,6 @@ public class StudyDashboard {
     public static void main(String[] args) throws IOException, InterruptedException {
         StudyDashboard studyDashboard = new StudyDashboard(15);
         studyDashboard.print();
-    }
-
-    private Participant findParticipant(String username, List<Participant> participants) {
-        Participant participant = null;
-        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
-            participant = new Participant(username);
-            participants.add(participant);
-        } else {
-            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-        }
-        return participant;
     }
 
     private void print() throws IOException, InterruptedException {
@@ -72,55 +57,17 @@ public class StudyDashboard {
         latch.await();
         service.shutdown();
 
-        try (FileWriter fileWriter = new FileWriter("participants.md");
-            PrintWriter writer = new PrintWriter(fileWriter)) {
-            participants.sort(Comparator.comparing(Participant::username));
+        new StudyPrinter(this.totalNumberOfEvents, participants).execute();
+    }
 
-            writer.print(header(participants.size()));
-
-            participants.forEach(p -> {
-                String markdownForHomework = getMarkdownForParticipant(p);
-                writer.print(markdownForHomework);
-            });
+    private Participant findParticipant(String username, List<Participant> participants) {
+        Participant participant = null;
+        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
+            participant = new Participant(username);
+            participants.add(participant);
+        } else {
+            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
         }
+        return participant;
     }
-
-    private String getMarkdownForParticipant(Participant p) {
-        return String.format("| %s %s | %.2f%% |\n", p.username(), checkMark(p, this.totalNumberOfEvents),
-                p.getRate(this.totalNumberOfEvents));
-    }
-
-    /**
-     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
-     * | --- | --- | --- | --- | --- |
-     */
-    private String header(int totalNumberOfParticipants) {
-        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
-
-        for (int index = 1; index <= this.totalNumberOfEvents; index++) {
-            header.append(String.format(" %d주차 |", index));
-        }
-        header.append(" 참석율 |\n");
-
-        header.append("| --- ".repeat(Math.max(0, this.totalNumberOfEvents + 2)));
-        header.append("|\n");
-
-        return header.toString();
-    }
-
-    /**
-     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
-     */
-    private String checkMark(Participant p, int totalEvents) {
-        StringBuilder line = new StringBuilder();
-        for (int i = 1 ; i <= totalEvents ; i++) {
-            if(p.homework().containsKey(i) && p.homework().get(i)) {
-                line.append("|:white_check_mark:");
-            } else {
-                line.append("|:x:");
-            }
-        }
-        return line.toString();
-    }
-
 }
