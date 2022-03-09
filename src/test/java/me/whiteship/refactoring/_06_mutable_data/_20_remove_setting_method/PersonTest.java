d07d379f1e396335c07f1d94719a2fe2eb202d54
weekbelt <vfrvfr4207@gmail.com>
2022-03-09 12:32:47 +0900

    리팩토링 20. 세터 제거하기

--- src/test/java/me/whiteship/refactoring/_06_mutable_data/_20_remove_setting_method/PersonTest.java
+++ src/test/java/me/whiteship/refactoring/_06_mutable_data/_20_remove_setting_method/PersonTest.java
@@ -1,15 +1,14 @@
 package me.whiteship.refactoring._06_mutable_data._20_remove_setting_method;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class PersonTest {
 
     @Test
     void person() {
-        Person person = new Person();
-        person.setId(10);
+        Person person = new Person(10);
         person.setName("keesun");
         assertEquals(10, person.getId());
         assertEquals("keesun", person.getName());
