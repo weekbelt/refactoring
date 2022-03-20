e830460707424c1cdb8605154b53b4b663e46b9d
weekbelt <vfrvfr4207@gmail.com>
2022-03-20 20:47:16 +0900

    리팩토링 32. 조건부 로직을 다형성으로 바꾸기

--- src/test/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/variation/VoyageRatingTest.java
+++ src/test/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/variation/VoyageRatingTest.java
@@ -1,30 +1,29 @@
 package me.whiteship.refactoring._11_primitive_obsession._32_replace_conditional_with_polymorphism.variation;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class VoyageRatingTest {
 
     @Test
     void westIndies() {
-        VoyageRating voyageRating = new VoyageRating(new Voyage("west-inides", 10),
-                List.of(new VoyageHistory("east-indies", 5),
-                        new VoyageHistory("west-indies", 15),
-                        new VoyageHistory("china", -2),
-                        new VoyageHistory("west-africa", 7)));
+        VoyageRating voyageRating = RatingFactory.createRating(new Voyage("west-inides", 10),
+            List.of(new VoyageHistory("east-indies", 5),
+                new VoyageHistory("west-indies", 15),
+                new VoyageHistory("china", -2),
+                new VoyageHistory("west-africa", 7)));
         assertEquals('B', voyageRating.value());
     }
 
     @Test
     void china() {
-        VoyageRating voyageRating = new VoyageRating(new Voyage("china", 10),
-                List.of(new VoyageHistory("east-indies", 5),
-                        new VoyageHistory("west-indies", 15),
-                        new VoyageHistory("china", -2),
-                        new VoyageHistory("west-africa", 7)));
+        VoyageRating voyageRating = RatingFactory.createRating(new Voyage("china", 10),
+            List.of(new VoyageHistory("east-indies", 5),
+                new VoyageHistory("west-indies", 15),
+                new VoyageHistory("china", -2),
+                new VoyageHistory("west-africa", 7)));
         assertEquals('A', voyageRating.value());
     }
 
