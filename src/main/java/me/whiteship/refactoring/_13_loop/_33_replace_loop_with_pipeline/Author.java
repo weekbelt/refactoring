026610f3bc0e22fc980d4db196fa8ac59bbe2137
weekbelt <vfrvfr4207@gmail.com>
2022-03-22 20:40:43 +0900

    리팩토링 33. 반복문을 파이프라인으로 바꾸기

--- src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
+++ src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -1,7 +1,9 @@
 package me.whiteship.refactoring._13_loop._33_replace_loop_with_pipeline;
 
-import java.util.ArrayList;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
+import java.util.Objects;
 
 public class Author {
 
@@ -15,15 +17,10 @@ public class Author {
     }
 
     static public List<String> TwitterHandles(List<Author> authors, String company) {
-        var result = new ArrayList<String> ();
-        for (Author a : authors) {
-            if (a.company.equals(company)) {
-                var handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
-        return result;
+        return authors.stream()
+            .filter(author -> author.company.equals(company))
+            .map(author -> author.twitterHandle)
+            .filter(Objects::isNull)
+            .collect(toList());
     }
-
 }
